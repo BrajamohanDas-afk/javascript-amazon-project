4bde90c8938c63471704d07e1eb63e7464c0f97c
BrajamohanDas-afk <Brajamohandas0390@gmail.com>
2025-12-02 04:40:24 +0530

    added the export for cart and other thing

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import { cart } from '../data/cart.js';
+import { cart, addToCart } from '../data/cart.js';
+import { products } from '../data/products.js';
 
 let productsHTML=''
 products.forEach((product)=>{
@@ -58,32 +59,21 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
 
+function updateCartQuntity(){
+    let cartQuantity = 0;
+    
+    cart.forEach((cartItem)=>{
+        cartQuantity += Number(cartItem.quantity);
+    });
+    
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button)=>{
         button.addEventListener('click',()=>{
             const productId = button.dataset.productId;
-            let matchedItem;
-            cart.forEach((item)=>{
-                if(productId===item.productId){
-                    matchedItem=item;
-                }
-            });
-            if(matchedItem){
-                matchedItem.quantity += 1;
-            }else{
-                cart.push({
-                    productId : productId,
-                    quantity : 1
-                });
-            }
-
-            let cartQuantity = 0;
-
-            cart.forEach((item)=>{
-                cartQuantity += Number(item.quantity);
-            });
-
-            document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
-
+            addToCart(productId);
+            updateCartQuntity();
         });
     });
