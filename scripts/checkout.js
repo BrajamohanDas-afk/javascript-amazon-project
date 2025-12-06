b80012903b87a038de4da2dff28f38ac34beb1d4
BrajamohanDas-afk <Brajamohandas0390@gmail.com>
2025-12-06 19:50:52 +0530

    made delivery option interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import {cart,
   removeFromCart, 
   calculateCartQuantity, 
-  updateQuantity} from '../data/cart.js';
+  updateQuantity,
+  updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -99,7 +100,10 @@ let html='';
     const isChecked = deliveryOption.id === 
     cartItem.deliveryOptionId;
     html+=`
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option"
+    data-product-id="${matchingProduct.id}"
+    data-delivery-option-id="${deliveryOption.id}"
+    >
       <input type="radio"
         ${isChecked ? 'checked' : ''}
         class="delivery-option-input"
@@ -185,3 +189,11 @@ document.querySelectorAll('.js-save-link')
       updateCartQuantity();
     })
   })
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element)=>{
+    element.addEventListener('click',()=>{
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    });
+  });
