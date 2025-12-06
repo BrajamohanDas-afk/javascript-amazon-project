75f9ebe14715fc19f8ab9f08c8972c44ed8c21fe
BrajamohanDas-afk <Brajamohandas0390@gmail.com>
2025-12-06 18:29:50 +0530

    added update in the checkout page and cart options

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,6 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, 
+  addToCart, 
+  calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -79,3 +81,8 @@ document.querySelectorAll('.js-add-to-cart')
       updateCartQuantity();
     });
   });
+
+const cartQuantity = calculateCartQuantity();
+
+document.querySelector('.js-cart-quantity')
+  .innerHTML = cartQuantity;
