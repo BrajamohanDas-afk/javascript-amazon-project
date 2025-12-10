95cc062945e04f79a01f491ce3bd67b6b7938b98
BrajamohanDas-afk <Brajamohandas0390@gmail.com>
2025-12-11 00:08:42 +0530

    created the oop of the cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,9 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js'
+
+
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
